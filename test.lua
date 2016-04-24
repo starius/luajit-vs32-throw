9b72b09f74f881820967760c78f9e87cb3cd96bb
Boris Nagaev <bnagaev@gmail.com>
2016-04-24 23:38:54 +0300

    flush after each print

--- test.lua
+++ test.lua
@@ -2,6 +2,7 @@ local throw = require 'throw'
 
 for i = 1, 100 do
     print(i)
+    io.stdout:flush()
     local status, error_message = pcall(throw)
     assert(not status)
     assert(error_message == 'Lua error')
